fcfb4b5765e4a6b9e91e0c97680ff7fc4c90ffe0
Thomastu1 <144470300+Thomastu1@users.noreply.github.com>
2024-03-06 20:28:14 +0100

    fix

--- cd-netlify-react-app/src/component/home.js
+++ cd-netlify-react-app/src/component/home.js
@@ -5,27 +5,31 @@ import profileImage from '../head.png'
 import cv from '../CV.jpg'
 
 
+
 const Home = () => {
-  return (
-    <div class="hoome">
-      <header>
-       
+    return (
+      <div className="hoome">
+        <header>
+          <h1 className="titre">Bienvenue sur mon Portfolio</h1>
+        </header>
   
-        <h1 className='titre'>Bienvenue sur mon Portfolio</h1>
-      </header>
-
-      <div className="image-container flex items-center justify-end">
-      <p class="h2 mr-4">Développeur web FrontEnd | En recherche d'une alternance pour Janvier 2024 <br/><br/>
-        Rythme : 3 jours entreprise (mercredi - vendredi) | 2 jours formation (lundi - mardi)  </p>
+        <div className="image-container flex items-center justify-end mt-8 sm:mt-0">
+          <p className="h2">
+            Développeur web FrontEnd | En recherche d'une alternance pour Janvier 2024
+            <br />
+            <br />
+            Rythme : 3 jours entreprise (mercredi - vendredi) | 2 jours formation (lundi - mardi)
+          </p>
           <img src={profileImage} alt="Profil" className="profile-image" />
-     
         </div>
-        <div className="cv-container">
-          <a href={cv} target="_blank" rel="noopener noreferrer">Voir mon CV</a>
-
-          </div>
-    </div>
-  );
-};
-
-export default Home;
+        <div className="cv-container mt-8 sm:mt-0">
+          <a href={cv} target="_blank" rel="noopener noreferrer">
+            Voir mon CV
+          </a>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
--- cd-netlify-react-app/src/component/nav.js
+++ cd-netlify-react-app/src/component/nav.js
@@ -4,9 +4,9 @@ import profileImage from '../logototu.png';
 
 const navigation = [
   { name: 'Acceuil', href: '#', current: true },
-  { name: 'Présentation', href: '#', current: false },
-  { name: 'Projets', href: '#', current: false },
-  { name: 'Contact', href: '#', current: false },
+  { name: 'Présentation', href: '#presentation', current: false },
+  { name: 'Projets', href: '#projets', current: false },
+  { name: 'Contact', href: '#contact', current: false },
 ];
 
 function classNames(...classes) {
@@ -15,69 +15,75 @@ function classNames(...classes) {
 
 export default function Nav() {
   return (
-    <Disclosure as="nav" className="black">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 flex items-center justify-between">
-            {/* Logo */}
-            <img className="logo" src={profileImage} alt="Profil" />
+    <Disclosure as="nav" className="fixed top-0 z-50 w-full bg-black text-white overflow-hidden">
+      {({ open, close }) => (
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex items-center justify-between">
 
-            {/* Navigation */}
-            <div className="hidden sm:flex space-x-4">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'rounded-md px-3 py-2 text-sm font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </a>
-              ))}
-            </div>
+          {/* Logo */}
+          <img className="logo" src={profileImage} alt="Profil" />
 
-            {/* Mobile menu button */}
-            <div className="sm:hidden">
-              <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Open main menu</span>
-                {open ? (
-                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                ) : (
-                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                )}
-              </Disclosure.Button>
-            </div>
+          {/* Mobile menu button */}
+          <div className="sm:hidden">
+            <Disclosure.Button
+              onClick={open ? close : undefined}
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            >
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              {open ? (
+                <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+              ) : (
+                <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+              )}
+            </Disclosure.Button>
           </div>
 
-          {/* Mobile menu */}
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+          {/* Navigation (hidden on small screens) */}
+          <div className="hidden sm:flex space-x-4 ml-6">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className={classNames(
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'rounded-md px-3 py-2 text-sm font-medium'
+                )}
+                aria-current={item.current ? 'page' : undefined}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+
+          {/* Mobile menu (hidden on larger screens) */}
+          <Disclosure.Panel className="sm:hidden fixed top-0 left-0 w-full h-full bg-black opacity-80">
+        <div className="flex items-center justify-center h-full">
+          <div className="flex flex-col items-center space-y-4">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                onClick={() => {
+                  close(); // Utilisez la fonction de fermeture close ici
+                }}
+                className={classNames(
+                  item.current
+                    ? 'text-white'
+                    : 'text-gray-300 hover:text-white',
+                  'text-lg font-medium'
+                )}
+                aria-current={item.current ? 'page' : undefined}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      </Disclosure.Panel>
+    </div>
+  )}
+</Disclosure>
   );
 }
