36f65a7eef7e3a8655144cfa184a71899b9fb9fc
Thomastu1 <144470300+Thomastu1@users.noreply.github.com>
2024-07-12 01:42:25 +0200

    a

--- cd-netlify-react-app/src/component/contact.js
+++ cd-netlify-react-app/src/component/contact.js
@@ -5,33 +5,18 @@ import Linkedin from '../linkedin.png';
 
 export default function Contact() {
   return (
-    
-    <div className="bg-[#8D73EA] py-24 sm:py-32">
-        <p> <h1>Me contacter</h1></p>
+    <div style={{ backgroundColor: '#8D73EA', paddingTop: '6rem', paddingBottom: '8rem' }}>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
-        </h2>
-        <div className="mx-auto mt-10 flex items-center justify-center">
+        <h1 className="text-center text-3xl font-bold leading-9 text-white">Me contacter</h1>
+        <div className="mx-auto mt-10 flex items-center justify-center space-x-8">
           <a href="mailto:tu.thomas01@gmail.com" className="w-20">
-            <img
-              className="w-full h-auto"
-              src={mail}
-              alt="Email"
-            />
+            <img className="w-full h-auto" src={mail} alt="Email" />
           </a>
           <a href="https://github.com/Thomastu1" className="w-20">
-            <img
-              className="w-full h-auto"
-              src={Github}
-              alt="GitHub"
-            />
+            <img className="w-full h-auto" src={Github} alt="GitHub" />
           </a>
           <a href="https://www.linkedin.com/in/thomas-tu-0194a3141/" className="w-20">
-            <img
-              className="w-full h-auto"
-              src={Linkedin}
-              alt="LinkedIn"
-            />
+            <img className="w-full h-auto" src={Linkedin} alt="LinkedIn" />
           </a>
         </div>
       </div>
