441f9ae6a87b6535bace78a84e2bcf71c73d66bd
Thomastu1 <144470300+Thomastu1@users.noreply.github.com>
2024-09-20 01:11:32 +0200

    Update App.js

--- cd-netlify-react-app/src/App.js
+++ cd-netlify-react-app/src/App.js
@@ -5,42 +5,48 @@ import Homea from './component/home';
 import Projet from './component/Projet';
 import Contact from './component/contact';
 
-
 function App() {
   return (
     <div className="App">
-<Nav/>
-<Homea/>
-<a href="#presentation" id="presentation"> </a>
-<br/>
-<br/>
-<br/>
-<br/>
-
-<p> <h1>Présentation</h1></p>
-<br/>
-<Pres/>
-<br/>
-<br/>
-<br/>
-<a href="#projets" id="projets"> </a>
-<br/>
-<p> <a href='https://www.figma.com/proto/DVBo9RubB4rUx0knXc9yHk/Portfolio-Totu?node-id=63-2014&node-type=frame&t=ckYTA8VtVmiEo5Ga-0&scaling=min-zoom&content-scaling=fixed&page-id=63%3A1632&starting-point-node-id=63%3A2014'<h1>Portfolio</h1></p>
-
-<p> <h1>Projets Web</h1></p>
-<br/>
-<Projet/> 
-<br/>
-
-<a href="#contact" id="contact"> </a>
-<br/>
-
-<Contact/>
-
-
-
-
-    </div>)
-    }
+      <Nav/>
+      <Homea/>
+      <a href="#presentation" id="presentation"> </a>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      
+      <p><h1>Présentation</h1></p>
+      <br/>
+      <Pres/>
+      <br/>
+      <br/>
+      <br/>
+      <a href="#projets" id="projets"> </a>
+      <br/>
+      
+      {/* Lien vers le portfolio sur Figma */}
+      <p>
+        <a 
+          href="https://www.figma.com/proto/DVBo9RubB4rUx0knXc9yHk/Portfolio-Totu?node-id=63-2014&node-type=frame&t=ckYTA8VtVmiEo5Ga-0&scaling=min-zoom&content-scaling=fixed&page-id=63%3A1632&starting-point-node-id=63%3A2014" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <h1>Portfolio</h1>
+        </a>
+      </p>
+      
+      <p><h1>Projets Web</h1></p>
+      <br/>
+      <Projet/> 
+      <br/>
+      
+      <a href="#contact" id="contact"> </a>
+      <br/>
+      
+      <Contact/>
+    </div>
+  );
+}
 
 export default App;
